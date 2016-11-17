1741aa0e41597ab9a00148fd9eee984dd52ca266
zenglen42 <gsnospampls@pobox.com>
2016-11-17 00:22:53 -0600

    wire up google maps + socrata endpoint example
    This is a nice little snippet that Jillian found here:
    
       http://jsfiddle.net/chrismetcalf/8m2Cs/
    
    It illustrates how to position markers on a google map
    based upon lat/lng data from another data source.
    
    The two data sources we're currently exploring are also available
    as an Socrata Open Data API (SODA) so this is higly relevant.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -38,6 +38,39 @@ function initMVC() {
 
 function cInit() {
 	console.log("cInit");
+	cDemoSocrataExample();
+}
+
+// Function: cDemoSocrataExample
+// Usage: cDemoSocrataExample();
+// -----------------------------
+// Courtesy: http://jsfiddle.net/chrismetcalf/8m2Cs/
+//
+// Populate a google map with markers based upon lat/lng data from
+// a json record fetched from endpoint.
+
+function cDemoSocrataExample() {
+	// Construct the catalog query string
+	url = 'http://data.ct.gov/resource/9k2y-kqxn.json?organization_type=Public%20School%20Districts&$$app_token=CGxaHQoQlgQSev4zyUh5aR5J3';
+
+	// Intialize our map
+	var center = new google.maps.LatLng(41.7656874, -72.680087);
+	var mapOptions = {
+		zoom: 8,
+		center: center
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	// Retrieve our data and plot it
+	$.getJSON(url, function initMap(data, textstatus) {
+		console.log(data);
+		$.each(data, function(i, entry) {
+			var marker = new google.maps.Marker({
+					position: new google.maps.LatLng(entry.location_1.latitude, entry.location_1.longitude),
+					map: map,
+					title: location.name});
+		});
+	});
 }
 
 //---------------------------------------------------------------------------
